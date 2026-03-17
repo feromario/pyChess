6c88e699435eb95c927b3daf34d270d576d0b194
Mario F <110302226+thelastmario@users.noreply.github.com>
2026-03-17 17:05:57 -0400

    Added constants.

---
+++ constans.py
@@ -0,0 +1,18 @@
+import pygame
+
+# ***** Board dimensions *****
+BOARD_SIZE       = 8
+SQUARE_SIZE      = 80
+WINDOW_SIZE      = BOARD_SIZE * SQUARE_SIZE
+
+# ***** Colors in RGB *****
+LIGHT_SQUARE     = (240, 217, 181)
+DARK_SQUARE      = (181, 136,  99)
+HIGHLIGHT        = ( 20,  85,  30, 128)
+SELECTED         = (255, 255,   0, 128)
+TEXT_COLOR       = (255, 255, 255)
+
+# ***** Piece id's used as dictionary keys *****
+WHITE            = "white"
+BLACK            = "black"
+PIECE_TYPES      = ['king', 'queen', 'rook', 'bishop', 'knight', 'pawn']
